cc4f3dc403d0f98fe847ac926da549eca6eaf2c3
jackskim <jackskim29@gmail.com>
2020-08-03 10:12:23 -0700

    Add TypeScript DLL with tests

--- doubly_linked_list/doublyLinkedList.test.ts
+++ doubly_linked_list/doublyLinkedList.test.ts
@@ -1,13 +1,174 @@
 import { ListNode, DoublyLinkedList } from "./doublyLinkedList";
 
-describe("DoublyLinkedList tests", () => {
+describe("ListNode tests", () => {
   const node = new ListNode(1);
-  const dll = new DoublyLinkedList(node);
+
+  test("delete", () => {
+    const node1 = new ListNode(3);
+    const node2 = new ListNode(4);
+    const node3 = new ListNode(5);
+
+    node1.next = node2;
+    node2.next = node3;
+    node2.prev = node1;
+    node3.prev = node2;
+
+    node2.delete();
+
+    expect(node1.next).toEqual(node3);
+    expect(node3.prev).toEqual(node1);
+  });
+
+  test("insert before", () => {
+    node.insertBefore(0);
+    expect(node.prev.value).toEqual(0);
+  });
+
+  test("insert after", () => {
+    node.insertAfter(2);
+    expect(node.next.value).toEqual(2);
+  });
+});
+
+describe("DoublyLinkedList tests", () => {
+  let node;
+  let dll;
+  beforeEach(() => {
+    node = new ListNode(1);
+    dll = new DoublyLinkedList(node);
+  });
 
   test("remove from tail", () => {
     dll.removeFromTail();
     expect(dll.head).toBeNull();
     expect(dll.tail).toBeNull();
     expect(dll.length).toEqual(0);
+
+    dll.addToTail(33);
+    expect(dll.head.value).toEqual(33);
+    expect(dll.tail.value).toEqual(33);
+    expect(dll.length).toEqual(1);
+    expect(dll.removeFromTail()).toEqual(33);
+    expect(dll.length).toEqual(0);
+
+    dll.addToTail(68);
+    expect(dll.length).toEqual(1);
+    expect(dll.removeFromTail()).toEqual(68);
+    expect(dll.length).toEqual(0);
+  });
+
+  test("remove from head", () => {
+    dll.removeFromHead();
+    expect(dll.head).toBeNull();
+    expect(dll.tail).toBeNull();
+    expect(dll.length).toEqual(0);
+
+    dll.addToHead(2);
+    expect(dll.head.value).toEqual(2);
+    expect(dll.tail.value).toEqual(2);
+    expect(dll.length).toEqual(1);
+    expect(dll.removeFromHead()).toEqual(2);
+    expect(dll.length).toEqual(0);
+
+    dll.addToHead(55);
+    expect(dll.length).toEqual(1);
+    expect(dll.removeFromHead()).toEqual(55);
+    expect(dll.length).toEqual(0);
+  });
+
+  test("Add to tail", () => {
+    dll.addToTail(1);
+    expect(dll.tail.value).toEqual(1);
+    expect(dll.length).toEqual(2);
+
+    dll.addToTail(30);
+    expect(dll.tail.prev.value).toBe(1);
+    expect(dll.tail.value).toEqual(30);
+    expect(dll.length).toEqual(3);
+
+    dll.addToTail(20);
+    expect(dll.tail.prev.value).toBe(30);
+    expect(dll.tail.value).toEqual(20);
+    expect(dll.length).toEqual(4);
+  });
+
+  test("Add to head", () => {
+    expect(dll.head.value).toEqual(1);
+
+    dll.addToHead(10);
+    expect(dll.head.value).toEqual(10);
+    expect(dll.head.next.value).toEqual(1);
+    expect(dll.length).toEqual(2);
+  });
+
+  test("Move to end", () => {
+    dll.addToHead(40);
+    expect(dll.tail.value).toEqual(1);
+    expect(dll.head.value).toEqual(40);
+
+    dll.moveToEnd(dll.head);
+    expect(dll.tail.value).toEqual(40);
+    expect(dll.tail.prev.value).toEqual(1);
+    expect(dll.length).toEqual(2);
+
+    dll.addToTail(4);
+    dll.moveToEnd(dll.head.next);
+    expect(dll.tail.value).toEqual(40);
+    expect(dll.tail.prev.value).toEqual(4);
+    expect(dll.length).toEqual(3);
+  });
+
+  test("Move to front", () => {
+    // Reset the DLL to one node, with value: 1
+    dll = new DoublyLinkedList(node);
+
+    dll.addToTail(3);
+    expect(dll.head.value).toEqual(1);
+    expect(dll.tail.value).toEqual(3);
+
+    dll.moveToFront(dll.tail);
+    expect(dll.head.value).toEqual(3);
+    expect(dll.head.next.value).toEqual(1);
+    expect(dll.length).toEqual(2);
+
+    dll.addToHead(29);
+    dll.moveToFront(dll.head.next);
+    expect(dll.head.value).toEqual(3);
+    expect(dll.head.next.value).toEqual(29);
+    expect(dll.length).toEqual(3);
+  });
+
+  test("delete", () => {
+    // Reset the DLL to one node, with value: 1
+    dll = new DoublyLinkedList(node);
+
+    dll.delete(node);
+    expect(dll.head).toBeNull();
+    expect(dll.head).toBeNull();
+    expect(dll.length).toEqual(0);
+
+    dll.addToTail(1);
+    dll.addToHead(9);
+    dll.addToTail(6);
+
+    dll.delete(dll.head);
+    expect(dll.head.value).toEqual(1);
+    expect(dll.tail.value).toEqual(6);
+    expect(dll.length).toEqual(2);
+
+    dll.delete(dll.head);
+    expect(dll.head.value).toEqual(6);
+    expect(dll.tail.value).toEqual(6);
+    expect(dll.length).toEqual(1);
+  });
+
+  test("get max", () => {
+    expect(dll.getMax()).toEqual(1);
+    dll.addToTail(100);
+    expect(dll.getMax()).toEqual(100);
+    dll.addToTail(55);
+    expect(dll.getMax()).toEqual(100);
+    dll.addToTail(101);
+    expect(dll.getMax()).toEqual(101);
   });
 });
--- doubly_linked_list/doublyLinkedList.ts
+++ doubly_linked_list/doublyLinkedList.ts
@@ -1,22 +1,24 @@
 interface Node {
   value: any;
-  prev: Node;
-  next: Node;
-}
-
-export class ListNode {
-  value: any;
   prev: Node | null;
   next: Node | null;
-  this: object;
+  delete: Function;
+}
 
-  constructor(value: any, prev, next) {
-    this.value = value;
-    this.prev = prev || null;
-    this.next = next || null;
-  }
+interface DLL {
+  head: Node | null;
+  tail: Node | null;
+  length: number | null;
+}
 
-  insertAfter(value: any) {
+export class ListNode implements Node {
+  constructor(
+    public value: any,
+    public prev: Node = null,
+    public next: Node = null
+  ) {}
+
+  insertAfter(value) {
     const currentNext = this.next;
     this.next = new ListNode(value, this, currentNext);
     if (currentNext) {
@@ -24,7 +26,7 @@ export class ListNode {
     }
   }
 
-  insertBefore(value: any) {
+  insertBefore(value) {
     const currentPrev = this.prev;
     this.prev = new ListNode(value, currentPrev, this);
     if (currentPrev) {
@@ -42,24 +44,22 @@ export class ListNode {
   }
 }
 
-export class DoublyLinkedList {
-  node: object | undefined;
-  head: Node;
-  tail: Node;
-  length: number;
+export class DoublyLinkedList implements DLL {
+  public head: Node | null;
+  public tail: Node | null;
+  public length: number;
 
-  constructor(node) {
-    this.head = node || null;
-    this.tail = node || null;
+  constructor(node: Node | null) {
+    this.head = node;
+    this.tail = node;
     this.length = 0;
     if (node !== null) {
       this.length = 1;
     }
   }
 
-  addToHead(value: any) {
+  addToHead(value) {
     const newNode = new ListNode(value, null, null);
-    this.length += 1;
 
     if (this.head) {
       newNode.next = this.head;
@@ -69,5 +69,145 @@ export class DoublyLinkedList {
       this.head = newNode;
       this.tail = newNode;
     }
+
+    this.length += 1;
+  }
+
+  removeFromHead() {
+    // Check if there is a head
+    if (this.head === null) {
+      return null;
+    }
+
+    // Store the current head
+    const currentHead = this.head;
+
+    if (this.head.next !== null) {
+      // If there's at next two nodes
+      this.head = this.head.next;
+      this.head.prev = null;
+    } else {
+      // If there's only one node
+      this.head = null;
+      this.tail = null;
+    }
+
+    this.length -= 1;
+
+    return currentHead.value;
+  }
+
+  addToTail(value) {
+    const newNode = new ListNode(value, null, null);
+
+    // If there is a tail
+    if (this.tail !== null) {
+      // Point the new node's prev to the current tail
+      newNode.prev = this.tail;
+
+      // Point the current tail's next to the new node
+      this.tail.next = newNode;
+
+      // Update our tail pointer
+      this.tail = newNode;
+    } else {
+      // Otherwise, this is the first node in the list
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
+  }
+
+  removeFromTail() {
+    // Check if there is a tail
+    if (this.tail === null) {
+      return null;
+    }
+
+    // Store the current tail
+    const currentTail = this.tail;
+
+    // If there's at least two nodes in the list
+    if (this.tail.prev !== null) {
+      // Set the tail to the node right before the tail
+      this.tail = this.tail.prev;
+      // Set the new tail's next pointer to null
+      this.tail.next = null;
+    } else {
+      // Otherwise there was only one node in the list
+      this.tail = null;
+      this.head = null;
+    }
+
+    this.length -= 1;
+
+    return currentTail.value;
+  }
+
+  delete(node: Node) {
+    // If the list is empty
+    if (this.head === null) return null;
+
+    // If there's only one node in the list
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    // If we have at least two nodes, and the node we want to delete is the head
+    if (node === this.head) {
+      this.head = node.next;
+      this.head.prev = null;
+    }
+
+    // If we have at least two nodes, and the node we want to delete is the tail
+    if (node === this.tail) {
+      this.tail = node.prev;
+      this.tail.next = null;
+    } else {
+      node.delete();
+    }
+
+    this.length -= 1;
+  }
+
+  moveToFront(node: Node) {
+    // Check if it's already the head
+    if (node === this.head) {
+      return null;
+    }
+
+    // Remove it from the list
+    this.delete(node);
+
+    // Add it to the head
+    this.addToHead(node.value);
+  }
+
+  moveToEnd(node: Node) {
+    // Check if it's already the tail
+    if (node === this.tail) {
+      return null;
+    }
+
+    // Remove it from the list
+    this.delete(node);
+
+    // Add it to the tail
+    this.addToTail(node.value);
+  }
+
+  getMax() {
+    let max = 0;
+    let node = this.head;
+
+    while (node !== null) {
+      if (node.value > max) {
+        max = node.value;
+      }
+      node = node.next;
+    }
+    return max;
   }
 }
