7f423aae5e18d48d52eae5de9e4187a00003dc23
jackskim <jackskim29@gmail.com>
2020-10-15 10:11:17 -0700

    Edit comments

--- algorithms/merging_ranges/merge_ranges.py
+++ algorithms/merging_ranges/merge_ranges.py
@@ -5,16 +5,16 @@ def merge_ranges(meetings):
     idx = 0
 
     while idx < len(meetings) - 1:
-        # If the end of the first meeting is equal to
-        # or later than the start of the second meeting
+        # If the end of the current meeting is equal to
+        # or later than the start of the next meeting
         if meetings[idx][1] >= meetings[idx+1][0]:
             # The end time of the merged meeting is the later end_time
             end_time = max(meetings[idx][1], meetings[idx+1][1])
 
-            # Replace the first meeting with the merged meeting
+            # Replace the current meeting with the merged meeting times
             meetings[idx] = (meetings[idx][0], end_time)
 
-            # Remove the second meeting from the list
+            # Remove the next meeting from the list
             meetings.pop(idx + 1)
         else:
             idx += 1
